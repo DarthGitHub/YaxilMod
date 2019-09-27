7cf54cf190dd81bdc1c23ec42ea45f5a59829b75
Darth Pixel <45312472+DarthPixelGitHub@users.noreply.github.com>
2019-09-27 19:11:46 +0100

    Just adding buffs for now

---
+++ Buffs/TamedPolarBearBuff.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace YaxilMod.Buffs
+{
+    public class TamedPolarBearBuff : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Polar Bear");
+            Description.SetDefault("A loyal companion with a cold heart.");
+            Main.buffNoTimeDisplay[Type] = true;
+            Main.buffNoSave[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.mount.SetMount(mod.MountType("TamedPolarBear"), player);
+            player.buffTime[buffIndex] = 10;
+        }
+    }
+}
